7327b7287712b55b9a2df2a94fce673174b2fedb
your name <charlieismylife2003@gmail.com>
2022-07-05 13:13:01 +0700

    config env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    MONGO_URI:
+      "mongodb+srv://tuankietcoder:01629402815@tuankietcoderblog.euzbd.mongodb.net/?retryWrites=true&w=majority",
+    MONGO_DB: "tuankietcoder_db",
+  },
+};
+
+module.exports = nextConfig;
