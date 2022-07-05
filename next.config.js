2a468fd430ddf9b1d42c5f5206f26a08998650d2
your name <charlieismylife2003@gmail.com>
2022-07-05 13:25:42 +0700

    env config

--- next.config.js
+++ next.config.js
@@ -3,12 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = {
-  env: {
-    MONGO_URI:
-      "mongodb+srv://tuankietcoder:01629402815@tuankietcoderblog.euzbd.mongodb.net/?retryWrites=true&w=majority",
-    MONGO_DB: "tuankietcoder_db",
-  },
-};
-
 module.exports = nextConfig;
